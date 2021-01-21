818595a2c3a2dc438327919133e0b1b24bfc5a56
pushlang <aapust@gmail.com>
2021-01-21 22:53:35 +0300

    server

--- server/main.go
+++ server/main.go
@@ -14,6 +14,8 @@ import (
 	"strings"
 )
 
+var manager = "http://127.0.0.1:8090"
+
 func mustParseMT(r *http.Request, h string) (string, map[string]string) {
 	t, p, err := mime.ParseMediaType(r.Header.Get(h))
 	if err != nil {
@@ -23,12 +25,15 @@ func mustParseMT(r *http.Request, h string) (string, map[string]string) {
 }
 
 func pathExec(fn string, tl string) string {
-	fmt.Println("filename:", fileName)
+	fmt.Println("file name:", fn)
 	p, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
-	return filepath.Dir(path) + "/" + fn + tl
+	fp := filepath.Dir(p) + "/" + fn + tl
+
+	fmt.Println("full path:", fp)
+	return fp
 }
 
 func main() {
@@ -53,16 +58,14 @@ func main() {
 
 				if fileName, ok := params["filename"]; ok {
 					fullPath := pathExec(fileName, ".copy")
-					fmt.Println("fullPath:", fullPath)
 					writeFile(fullPath, partData)
 
-					//prepare the reader instances to encode
 					values := map[string]io.Reader{
-						pparams["name"]: mustOpen(fullPath),
-						"username":      strings.NewReader("user01"),
+						params["name"]: mustOpen(fullPath),
+						"username":     strings.NewReader("user01"),
 					}
-					err = Upload(&http.Client{},
-						"http://127.0.0.1:8090", values)
+					err = Upload(&http.Client{}, manager, values)
+
 					if err != nil {
 						panic(err)
 					}
@@ -78,7 +81,6 @@ func main() {
 }
 
 func Upload(client *http.Client, url string, values map[string]io.Reader) (err error) {
-	// Prepare a form that you will submit to that URL.
 	var b bytes.Buffer
 	w := multipart.NewWriter(&b)
 	for key, r := range values {
@@ -86,45 +88,39 @@ func Upload(client *http.Client, url string, values map[string]io.Reader) (err e
 		if x, ok := r.(io.Closer); ok {
 			defer x.Close()
 		}
-		// Add an image file
 		if x, ok := r.(*os.File); ok {
 			if fw, err = w.CreateFormFile(key, x.Name()); err != nil {
-				return err
+				return
 			}
 		} else {
-			// Add other fields
 			if fw, err = w.CreateFormField(key); err != nil {
-				return err
+				return
 			}
 		}
 		if _, err = io.Copy(fw, r); err != nil {
-			return err
+			return
 		}
 
 	}
-	// Don't forget to close the multipart writer.
-	// If you don't close it, your request will be missing the terminating boundary.
 	w.Close()
-
-	// Now that you have a form, you can submit it to your handler.
+	postForm(url, b, w)
+}
+func postForm(url string, b bytes.Buffer, w multipart.NewWriter) (err error) {
 	req, err := http.NewRequest("POST", url, &b)
 	if err != nil {
-		return err
+		return
 	}
-	// Don't forget to set the content type, this will contain the boundary.
 	req.Header.Set("Content-Type", w.FormDataContentType())
 
-	// Submit the request
 	res, err := client.Do(req)
 	if err != nil {
-		return err
+		return
 	}
 
-	// Check the response
 	if res.StatusCode != http.StatusOK {
 		err = fmt.Errorf("bad status: %s", res.Status)
 	}
-	return err
+	return
 }
 
 func mustOpen(f string) *os.File {
